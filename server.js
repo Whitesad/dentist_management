6c4ba9207f09837b87c4c770baaa1ee98d4de563
whitesad <whitesad_01@outlook.com>
2019-12-23 17:58:32 +0800

    complete the query api

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.keys=['yang']
 var sql_ctrl=new require('./controller/sql_ctrl')()
 var json_ctrl=new require('./controller/json_ctrl')()
 
+// create application/x-www-form-urlencoded decoder
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 //===============================================
 //*************https server**********************
@@ -32,14 +35,8 @@ httpsServer.listen(PORT, function() {
 
 //===============================================
 //*********login api*****************************
+//登录api，登录后会创建加密cookie保证登录状态为1 week
 //===============================================
-
-
-// create application/x-www-form-urlencoded decoder
-var bodyParser = require('body-parser');
-
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
 
 app.post('/login',urlencodedParser,function (req,res) {
     if(req.protocol === 'https') {
@@ -50,8 +47,8 @@ app.post('/login',urlencodedParser,function (req,res) {
         //var login_status = sql_ctrl.login(acconut)
 
         //test code
-        // login_status={status:'success'}
-        // account={username:"test02",identity_ID: '123'}
+        login_status={status:'success'}
+        account={username:"test02",identity_ID: '123'}
 
         if(login_status.status=='success')
         {
@@ -67,35 +64,50 @@ app.post('/login',urlencodedParser,function (req,res) {
     }
 })
 
-
 //===============================================
 //***************register api********************
+//注册api，分为医生与病人
 //===============================================
-app.post('/register',urlencodedParser,function (req,res) {
+
+//病人注册api
+app.post('/register/patient',urlencodedParser,function (req,res) {
     if(req.protocol === 'https') {
-        console.log("A connect try to register")
+        console.log("A connect try to register as patient")
         console.log("host:"+req.headers.host+"  servername:"+req.connection.servername)
 
         var register_info=new json_ctrl(res.query)
         console.log(JSON.stringify(register_info))
 
-        var register_status
-
-        if(register_info.type==='doctor'){
-            register_status=sql_ctrl.register_doctor(register_info)
-        }else if(register_info.type==='patient'){
-            register_status=sql_ctrl.register_patient(register_info)
-        }
+        var register_status=sql_ctrl.register_patient(register_info)
 
         console.log(register_status);
         console.log("\n\n")
-        res.end(JSON.stringify(register_status));
+        res.end(register_status);
+    }
+})
+
+//医生注册api
+app.post('/register/doctor',urlencodedParser,function (req,res) {
+    if(req.protocol === 'https') {
+        console.log("A connect try to register as patient")
+        console.log("host:"+req.headers.host+"  servername:"+req.connection.servername)
+
+        var register_info=new json_ctrl(res.query)
+        console.log(JSON.stringify(register_info))
+
+        var register_status=sql_ctrl.register_doctor(register_info)
+
+        console.log(register_status);
+        console.log("\n\n")
+        res.end(register_status);
     }
 })
 
 //===============================================
 //***************query api**********************
 //===============================================
+
+//查询个人信息
 app.post('/query/personal_info',urlencodedParser,function (req,res) {
     if(req.protocol === 'https') {
         //if is login
@@ -118,11 +130,211 @@ app.post('/query/personal_info',urlencodedParser,function (req,res) {
 
                 console.log(personal_info)
                 console.log("\n\n")
-                return personal_info
+                res.send(personal_info)
             }
         }else
         {
-            console.log("invalid query:no login!")
+            console.log("invalid personal info query:no login!")
+            console.log("\n\n")
+            res.redirect('./login')
+        }
+    }
+})
+
+//查询诊疗记录
+app.post('/query/treat_record',urlencodedParser,function (req,res) {
+    if(req.protocol === 'https') {
+        //if is login
+        if(req.signedCookies['is_login']=='true')
+        {
+            //need try catch
+            var account={
+                username: req.signedCookies['username'],
+                identity_ID:req.signedCookies['identity_ID']
+            }
+            console.log("username:"+account.username+"  identity_ID:"+account.identity_ID+"  try to query treat_record info.")
+            console.log("host:" + req.headers.host + "  servername:" + req.connection.servername)
+
+            var treat_query_info = new json_ctrl(res.query)
+            console.log(JSON.stringify(query_info))
+
+            if(treat_query_info.type=='treat_list')
+            {
+                var treat_list_info=sql_ctrl.query_treat_list_info(account);
+
+                console.log(JSON.stringify(treat_list_info))
+                console.log("\n\n")
+                res.send(treat_list_info)
+            }else if(treat_query_info.type=='treat_record')
+            {
+                var record={
+                    record_ID:treat_query_info.value.record_ID
+                }
+
+                var treat_record_info=sql_ctrl.query_treat_record_info(record);
+
+                console.log(JSON.stringify(treat_record_info));
+                console.log("\n\n")
+                res.send(treat_record_info)
+            }
+        }else
+        {
+            console.log("invalid treat record query:no login!")
+            console.log("\n\n")
+            res.redirect('./login')
+        }
+    }
+})
+
+//查询医生信息
+app.post('/query/doctor_info',urlencodedParser,function (req,res) {
+    if(req.protocol === 'https') {
+        //if is login
+        if(req.signedCookies['is_login']=='true')
+        {
+            //need try catch
+            var account={
+                username: req.signedCookies['username'],
+                identity_ID:req.signedCookies['identity_ID']
+            }
+            console.log("username:"+account.username+"  identity_ID:"+account.identity_ID+"  try to query doctor info.")
+            console.log("host:" + req.headers.host + "  servername:" + req.connection.servername)
+
+            var doctor_query_info = new json_ctrl(res.query)
+            console.log(JSON.stringify(doctor_query_info))
+
+            if(doctor_query_info.type=='doctor_list')
+            {
+                var doctor_list_info=sql_ctrl.query_doctor_list_info();
+
+                console.log(JSON.stringify(doctor_list_info))
+                console.log("\n\n")
+                res.send(doctor_list_info)
+            }else if(doctor_query_info.type=='doctor_detail')
+            {
+                var doctor={
+                    doctor_name:doctor_query_info.value.doctor_name,
+                    doctor_job_ID:doctor_query_info.value.doctor_job_ID
+                }
+
+                var doctor_info=sql_ctrl.query_doctor_info(doctor);
+
+                console.log(JSON.stringify(doctor_info));
+                console.log("\n\n")
+                res.send(doctor_info)
+            }
+        }else
+        {
+            console.log("invalid doctor info query:no login!")
+            console.log("\n\n")
+            res.redirect('./login')
+        }
+    }
+})
+
+//查询公告信息
+app.post('/query/annouce_info',urlencodedParser,function (req,res) {
+    if(req.protocol === 'https') {
+        console.log("A connect try to query the annouce")
+        console.log("host:" + req.headers.host + "  servername:" + req.connection.servername)
+
+        var annouce_info=sql_ctrl.query_announce_info()
+        console.log(JSON.stringify(annouce_info))
+        console.log("\n\n")
+        res.send(annouce_info)
+    }
+})
+
+//查询预约信息
+app.post('/query/doctor_info',urlencodedParser,function (req,res) {
+    if(req.protocol === 'https') {
+        //if is login
+        if(req.signedCookies['is_login']=='true')
+        {
+            //need try catch
+            var account={
+                username: req.signedCookies['username'],
+                identity_ID:req.signedCookies['identity_ID']
+            }
+            console.log("username:"+account.username+"  identity_ID:"+account.identity_ID+"  try to query reserve info.")
+            console.log("host:" + req.headers.host + "  servername:" + req.connection.servername)
+
+            var reserve_query_info = new json_ctrl(res.query)
+            console.log(JSON.stringify(reserve_query_info))
+
+            if(reserve_query_info.type=='reserve_list')
+            {
+                var reserve_list_info=sql_ctrl.query_reserve_list_info(account);
+
+                console.log(JSON.stringify(reserve_list_info))
+                console.log("\n\n")
+                res.send(reserve_list_info)
+            }else if(reserve_query_info.type=='reserve_detail')
+            {
+                var reserve={
+                    reserve_ID:doctor_query_info.value.reserve_ID
+                }
+                var reserve_info=sql_ctrl.query_reserve_info(reserve);
+
+                console.log(JSON.stringify(reserve_info));
+                console.log("\n\n")
+                res.send(reserve_info)
+            }
+        }else
+        {
+            console.log("invalid reserve record query:no login!")
+            console.log("\n\n")
+            res.redirect('./login')
+        }
+    }
+})
+
+
+//===============================================
+//***************update api**********************
+//提供修改对数据库进行操作的权限，现在主要是修改个人信息
+//===============================================
+
+
+app.post('/update/doctor_info',urlencodedParser,function (req,res) {
+    if(req.protocol === 'https') {
+        //if is login
+        if(req.signedCookies['is_login']=='true')
+        {
+            //need try catch
+            var account={
+                username: req.signedCookies['username'],
+                identity_ID:req.signedCookies['identity_ID']
+            }
+            console.log("username:"+account.username+"  identity_ID:"+account.identity_ID+"  try to query doctor info.")
+            console.log("host:" + req.headers.host + "  servername:" + req.connection.servername)
+
+            var doctor_query_info = new json_ctrl(res.query)
+            console.log(JSON.stringify(doctor_query_info))
+
+            if(doctor_query_info.type=='doctor_list')
+            {
+                var doctor_list_info=sql_ctrl.query_doctor_list_info();
+
+                console.log(JSON.stringify(doctor_list_info))
+                console.log("\n\n")
+                res.send(doctor_list_info)
+            }else if(doctor_query_info.type=='doctor_detail')
+            {
+                var doctor={
+                    doctor_name:doctor_query_info.value.doctor_name,
+                    doctor_job_ID:doctor_query_info.value.doctor_job_ID
+                }
+
+                var doctor_info=sql_ctrl.query_doctor_info(doctor);
+
+                console.log(JSON.stringify(doctor_info));
+                console.log("\n\n")
+                res.send(doctor_info)
+            }
+        }else
+        {
+            console.log("invalid doctor info query:no login!")
             console.log("\n\n")
             res.redirect('./login')
         }
