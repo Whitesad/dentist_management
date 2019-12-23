343c2a4467a15d91c9994d82bcdf52d32f0cd71d
whca <www.wxmqq@163.com>
2019-12-23 10:17:44 +0800

    image_ctrl

--- controller/image_ctrl.js
+++ controller/image_ctrl.js
@@ -2,13 +2,13 @@ var fs = require('fs')
 var path = require('path')
 var crypto = require('crypto')
 var images = require('images')
-/*image_ctrl(posterData) 保存文件，如果是图片会进行压缩，返回promise，内容为文件路径
+/*savefile(posterData) 保存文件，如果是图片会进行压缩，返回promise，内容为文件路径
 * posterData post的文件，enctype="multipart/form-data"，postData为req.files.uploadPoster
 * promise获取方法
 * image_ctrl(req.files.file).then(newFilepath => {
         console.log(newFilepath)
     })*/
-function image_ctrl(posterData) {
+exports.savefile = function savefile(posterData) {
     var promise = new Promise(function (resolve, reject) {
         var filePath = posterData.path
         if(filePath){
@@ -52,12 +52,10 @@ function image_ctrl(posterData) {
     })
     promise.then(function (value) {
         return value
-    }, function (value) {
-
     })
     return promise
 }
-//保存图片并压缩
+/*保存图片并压缩*/
 function saveimage(type, data, resolve, newFilepath) {
     fs.writeFile(newFilepath, data, function (err) {
         if(newFilepath){
@@ -69,4 +67,30 @@ function saveimage(type, data, resolve, newFilepath) {
         }
     })
 }
-module.exports=image_ctrl
+/*读取图像，设置图像宽高，如果height未指定，则根据当前宽高等比缩放，如果width,height均未指定，则不做操作，返回promise，内容为文件路径*/
+exports.readfile = function readfile(imagepath, width, height){
+    var promise = new Promise(function (resolve, reject) {
+        if (imagepath) {
+            var type = imagepath.split('.')[imagepath.split('.').length - 1]
+            if((type == 'jpeg' || type == 'png' || type == 'jpg') && width != undefined) {
+                var timestamp = Date.now()
+                var Hash = crypto.createHash('md5')
+                var poster = Hash.update(imagepath + timestamp).digest('hex') + '.' + type
+                var newFilepath = path.join(__dirname, '/tmp/' + poster)
+                if (height != undefined) {
+                    images(imagepath).size(width, height).save(newFilepath)
+                } else {
+                    images(imagepath).size(width).save(newFilepath)
+                }
+                resolve(newFilepath)
+            }
+            else{
+                resolve(imagepath)
+            }
+        }
+    })
+    promise.then(function (value) {
+        return value
+    })
+    return promise
+}
